7492b6d9b81d81aa2fd74af9195f99666ceb0ed4
Zhou Hua <zhouhua015@gmail.com>
2021-11-22 08:52:21 +0800

    Fix shellcheck in syntastic

--- nvim/lua/my/lang/sh.lua
+++ nvim/lua/my/lang/sh.lua
@@ -3,7 +3,7 @@ return function(packer)
         'scrooloose/syntastic',
         ft = 'sh',
         config = function()
-            vim.g.syntastic_sh_checkers = 'shellcheck'
+            vim.g.syntastic_sh_checkers = { 'shellcheck' }
             vim.g.syntastic_sh_shellcheck_args = '-x'
         end,
     }
